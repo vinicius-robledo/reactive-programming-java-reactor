21ce9e508d0e3af6265543c6ce2c699ee6552b1a
vinicius-robledo <vinicius.robledo@fatec.sp.gov.br>
2022-07-06 11:55:20 -0300

    included Functional and Imperative Examples

--- src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
+++ src/main/java/com/learnreactiveprogramming/functional/FunctionalExample.java
@@ -1,4 +1,23 @@
 package com.learnreactiveprogramming.functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalExample {
+    public static void main(String[] args) {
+        var namesList = List.of("alex", "ben", "chloe", "adam", "adam");
+        var newNamesList = namesGreaterThanSize(namesList, 3);
+        System.out.println("newNamesList : " + newNamesList);
+    }
+
+    private static List<String> namesGreaterThanSize(List<String> namesList, int size) {
+        return namesList
+            .parallelStream()
+            .filter(s -> s.length() > size)
+            .map(String::toUpperCase)
+            .distinct()
+            .sorted()
+            .collect(Collectors.toList());
+
+    }
 }
